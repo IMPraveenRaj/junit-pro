51b4e02e28f8ed401af40a6d1672801d0f023e39
IMPraveenRaj <impraveen13@gmail.com>
2024-11-30 12:49:32 +0100

    unit testing prep work

---
+++ unit-testing-prep/src/test/java/victor/testing/prework/NuclearServiceTest.java
@@ -0,0 +1,10 @@
+package victor.testing.prework;
+
+import org.junit.jupiter.api.Test;
+
+public class NuclearServiceTest {
+   @Test
+   public void test() {
+      new NuclearService().interpolateQuantum(0,0); // TODO test me
+   }
+}
